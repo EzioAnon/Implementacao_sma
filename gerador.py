a689b4d53d94f6bd15487604d7156f4dd25a76e2
EzioAnon <dpsamaia@gmail.com>
2024-07-08 20:17:53 -0300

    Função cubica nao acha raiz ainda

--- gerador.py
+++ gerador.py
@@ -1,92 +1,110 @@
-import spade
 from spade.agent import Agent
 from spade.behaviour import CyclicBehaviour
-from spade.template import Template
+from spade.behaviour import OneShotBehaviour
 from spade.message import Message
 import random
+import asyncio
 
 class Gerador(Agent):
-    
-    grau = 1
-    x = random.randint(-1000,1000)
-
-
-    if grau == 1:
-        a = random.randint(-100,100)
-        b = -1*(a*x)
-    elif grau == 2:
-        a = random.randint(-100,100)
-        b = random.randint(-100,100)
-        c = -1*(a*x**2 + b*x)
-    else:
-        a = random.randint(-100,100)
-        b = random.randint(-100,100)
-        c = random.randint(-100,100)
-        d = -1*(a*x**3 + b*x**2 + c*x)
-    
-    class funcao_grau(CyclicBehaviour):
-     async def run(self):
-        res = await self.receive(timeout=5)
-        if res:
-            x = float(res.body)
-            if Gerador.grau == 1:
-                y = Gerador.a * x + Gerador.b
-            elif Gerador.grau == 2:
-                y = Gerador.a * x**2 + Gerador.b * x + Gerador.c
-            else:
-                y = Gerador.a * x**3 + Gerador.b * x**2 + Gerador.c * x + Gerador.d
-
-            print(f"Enviou para {res.sender} f({res.body}) = {y} => {int(y)}")
-            msg = Message(to=str(res.sender))
-            msg.set_metadata("performative", "inform")
-            msg.body = str(int(y))
-            await self.send(msg)
-    
-    class tipo_funcao(CyclicBehaviour):
+    class GrauFuncao(OneShotBehaviour):
         async def run(self):
-            msg = await self.receive(timeout=5)
+            # grau = random.randint(1, 3) 
+            grau =3
+            print(f"Grau da função gerado pelo Gerador: {grau}")
+
+            x = random.randint(-1000, 1000)
+            print(f"X da Função: {x}")
+
+            msg = await self.receive(timeout=10)
             if msg:
-                resposta = f"{Gerador.grau}grau"
-                print(f"Respondeu para {msg.sender} com {resposta}")
-                resposta_msg = Message(to=str(msg.sender))
-                resposta_msg.set_metadata("performative", "inform")
-                resposta_msg.body = resposta
-                await self.send(resposta_msg)
-    
+                print(f"Mensagem recebida pelo Gerador: {msg.body}")
+                response = Message(to=str(msg.sender), body=str(int(grau)), thread=msg.thread)
+                response.set_metadata("performative", "inform")
+                await self.send(response)
+                print("Grau enviado")
+            
+            if grau == 1:
+                a = random.randint(-100, 100)
+                b = -1 * (a * x)
+                print(f"Função 1 grau: {a}x^2 + ({b}x) ")
+                self.agent.add_behaviour(self.agent.Grau1(a, b, x))
+            elif grau == 2:
+                a = random.randint(-100, 100)
+                b = random.randint(-100, 100)
+                c = -1 * (a * x**2 + b * x)
+                print(f"Função 2 grau: {a}x^2 {b}x {c}")
+                self.agent.add_behaviour(self.agent.Grau2(a, b, c, x))
+            elif grau == 3:
+                a = random.randint(-100, 100)
+                b = random.randint(-100, 100)
+                c = random.randint(-100, 100)
+                d = -1 * (a * x**3 + b * x**2 + c * x)
+                print(f"Função 3 grau: {a}x^3 + ({b}x^2) +  ({c}x) + ({d})")
+                self.agent.add_behaviour(self.agent.Grau3(a, b, c, d, x))    
+
+    class Grau1(CyclicBehaviour):
+        def __init__(self, a, b, x):
+            super().__init__()
+            self.a = a
+            self.b = b
+            self.x = x
+
+        async def run(self):
+            msg = await self.receive(timeout=20)
+            if msg:
+             x_resolvedor = float(msg.body)  
+             resultado = self.a * x_resolvedor + self.b  
+             res = Message(to=str(msg.sender), body=str(int(resultado)), thread=msg.thread)
+             await self.send(res)
+
+             
+
+    class Grau2(CyclicBehaviour):
+        def __init__(self, a, b, c, x):
+            super().__init__()
+            self.a = a
+            self.b = b
+            self.c = c
+            self.x = x
+            
+
+        async def run(self):
+            msg = await self.receive(timeout=20)
+            if msg:
+             x_resolvedor = float(msg.body)
+             resultado = self.a * x_resolvedor**2 + self.b * x_resolvedor + self.c  
+             res = Message(to=str(msg.sender), body=str(int(resultado)), thread=msg.thread)
+             await self.send(res)
+
+    class Grau3(CyclicBehaviour):
+        def __init__(self, a, b, c, d, x):
+            super().__init__()
+            self.a = a
+            self.b = b
+            self.c = c
+            self.d = d
+            self.x = x
+
+        async def run(self):
+            msg = await self.receive(timeout=20)
+            if msg: 
+             x_resolvedor = float(msg.body)  
+             resultado = self.a * x_resolvedor**3 + self.b * x_resolvedor**2 + self.c * x_resolvedor + self.d
+             res = Message(to=str(msg.sender), body=str(int(resultado)), thread=msg.thread)
+             await self.send(res)
+  
+            
+
     async def setup(self):
-        t = Template()
-        t.set_metadata("performative", "subscribe")
+        print("Gerador iniciando...")
+        self.add_behaviour(self.GrauFuncao())
 
-        tf = self.funcao_grau()
-        print(f"Funcao de {Gerador.grau}o grau: ", Gerador.x)
-        msg = Message()
-        msg.set_metadata("performativa","inform")
-        if Gerador.grau == 1:
-            print(f"Funcao: {Gerador.a}x + ({Gerador.b})")
-            msg.to = "ezio_resolvedor1@jix.im"
-            msg.body = "Função de 1oGrau"
-        elif Gerador.grau == 2:
-            print(f"Funcao: {Gerador.a}x^2 + ({Gerador.b}x) + ({Gerador.c})")
-            msg.to = "ezioanon_resolvedor2@jix.im"
-            msg.body = "Função de 2oGrau"
-        else:
-            print(f"Funcao: {Gerador.a}x^3 + ({Gerador.b}x^2) + ({Gerador.c}x) + ({Gerador.d})")
-            msg.to = "ezio_resolvedor3@jix.im"
-            msg.body = "Função de 3oGrau"
-
-        self.add_behaviour(tf, t)
-
-        ft = self.tipo_funcao()
-        template = Template()
-        template.set_metadata("performative", "request")
-        self.add_behaviour(ft, template)
-    
-    
-    
-    
 async def main():
-    gerador = Gerador("ezioanon@jix.im", "senhaforte")
-    await gerador.start()
+    generator_agent = Gerador("ezioanon@jix.im", "senhaforte")
+    await generator_agent.start()
+    print("Gerador iniciado")
+
+    await asyncio.Event().wait()
 
 if __name__ == "__main__":
-    spade.run(main())
+    asyncio.run(main())
--- resolvedor_2.py
+++ resolvedor_2.py
@@ -1,49 +1,177 @@
-import spade
 from spade.agent import Agent
-from spade.behaviour import CyclicBehaviour
+from spade.behaviour import CyclicBehaviour,OneShotBehaviour
 from spade.message import Message
 import random
-class Resolvedor_2grau(Agent):
-   class resolver(CyclicBehaviour):
+import asyncio
+import numpy as np
+
+class Resolvedor(Agent):
+    class Grau(OneShotBehaviour):
         async def run(self):
-            x = []
-            y = []
-            i = 0
             
-            while True:
-                x.append(random.randint(-1000, 1000))
-                msg = Message(to="ezioanon@jix.im")
-                msg.set_metadata("performative", "inform")
+            msg = Message(to="ezioanon@jix.im", body="Qual o grau da Função?", thread="unique-thread-id")
+            msg.set_metadata("performative", "request")
+            await self.send(msg)
+            print("Mensagem enviada para o Gerador")
 
-                msg.body = str(x[i])
-                await self.send(msg)
+            
+            response = await self.receive(timeout=20)
+            if response:
+                grau = int(response.body)
+                print(f"Grau recebido do Gerador: {grau}")
 
-                res = await self.receive(timeout=5)
-                if res:
-                    y.append(int(res.body))
-                    if y[i] == 0:
-                        await self.agent.stop()
+                
+                if grau == 1:
+                    self.agent.add_behaviour(self.agent.Grau1())
+                elif grau == 2:
+                    self.agent.add_behaviour(self.agent.Grau2())
+                elif grau == 3:
+                    self.agent.add_behaviour(self.agent.Grau3())
+            
+    class Grau1(CyclicBehaviour):
+        async def on_start(self):
+            self.i = 0
+            self.y = []
+            self.x = []
 
-                    if i == 1:
-                        x_zero = x[1] - (((x[1] - x[0]) ** 2 * (y[1]- y[2]) - (x[1] - x[2])** 2 * (y[1]- y[0]))/2*((x[1]- x[0])* (y[1]-y[2]) - (x[1] - x[2]* (y[1]- y[0])) ))# Interpolação quadratica
-                        msg = Message(to="ezioanon@jix.im")
-                        msg.set_metadata("performative", "inform")
-                        msg.body = str(x_zero)
-                        await self.send(msg)
-                    i += 1
+        async def run(self):
+            if self.i > 1:
+                self.x.append(self.x[0] - ((self.x[1] - self.x[0]) * self.y[0]) / (self.y[1] - self.y[0]))  # Lógica para primeiro grau
+                x_zero = self.x[self.i]
+            else:
+                self.x.append(random.randint(-1000, 1000))
+                x_zero = self.x[self.i]
 
-   async def setup(self):
-        b = self.resolver()
-        self.add_behaviour(b)
+            print(f"Enviando valor {x_zero} para função de 1º grau")
 
+            msg = Message(to="ezioanon@jix.im", body=str(float(x_zero)), thread="unique-thread-id")
+            await self.send(msg)
 
-    
-    
-    
+            response = await self.receive(timeout=20)
+            if response:
+                self.y.append(int(response.body))
+                print(f"Resultado da função de 1º grau para {self.x[self.i]}: {self.y[self.i]}")
+                if self.y[self.i] == 0:
+                    print("Resultado zero encontrado!")
+                    await self.agent.stop()
+                    self.kill()
+
+                self.i += 1
+                        
+
+    class Grau2(CyclicBehaviour):
+        async def on_start(self):
+            self.i = 0
+            self.y = []
+            self.x = []
+            self.a = 0
+            self.b = 0
+            self.c = 0
+            self.raiz_x = 0
+
+        async def run(self):
+            if self.i == 3:
+                self.a = (self.y[0]- self.y[2] - (((self.y[1]-self.y[2]) * (self.x[0] - self.x[2]))/(self.x[1] - self.x[2]))) / ((self.x[0]**2 - self.x[2]**2) - (((self.x[1]**2 - self.x[2]**2)*(self.x[0] - self.x[2]))/(self.x[1] - self.x[2])))
+                self.b = ((self.y[1] - self.y[2])- self.a * (self.x[1]**2 - self.x[2]**2))/(self.x[1] - self.x[2])
+                self.c = self.y[2] - self.a * self.x[2]**2 - self.b*self.x[2]
+                delta = self.b**2 - 4 * (self.a)*(self.c)
+                if self.raiz_x == 0:
+                 self.x.append((-self.b + np.sqrt(delta))/(2 * self.a))
+                 x_zero = self.x[self.i]
+                 self.raiz_x +=1
+                 
+                else:
+                 self.x.append((-self.b - np.sqrt(delta))/(2 * self.a))
+                 x_zero = self.x[self.i]
+                 
+                
+                print(f"a:{self.a}, b:{self.b}, c:{self.c}, raiz_1: {x_zero}")
+                
+                
+            else:
+                self.x.append(random.randint(-1000, 1000))
+                x_zero = self.x[self.i]
+
+            print(f"Enviando valor {x_zero} para função de 2º grau")
+
+            msg = Message(to="ezioanon@jix.im", body=str(float(x_zero)), thread="unique-thread-id")
+            await self.send(msg)
+
+            response = await self.receive(timeout=20)
+            if response:
+                self.y.append(int(response.body))
+                print(f"Resultado da função de 2º grau para {self.x[self.i]}: {self.y[self.i]}")
+                if self.y[self.i] == 0:
+                    print("Resultado zero encontrado!")
+                    await self.agent.stop()
+                    self.kill()
+                if self.i < 5: self.i += 1
+                else: 
+                    self.i = 0
+                    self.x = []
+                    self.y = []
+                    self.raiz_x = 0
+
+                        
+                        
+
+    class Grau3(CyclicBehaviour):
+        async def on_start(self):
+            self.i = 0
+            self.y = []
+            self.x = []
+            self.a = 0
+            self.b = 0
+            self.c = 0
+            self.d = 0
+            self.raiz_x = 0
+
+        async def run(self):
+            if self.i==4:
+                x = np.array(self.x)
+                y = np.array(self.y)
+                matriz = np.column_stack([x**3,x**2,x,np.ones_like(x)])
+                coeficientes = np.linalg.solve(matriz,y)
+                self.a,self.b,self.c,self.d = coeficientes
+                q = 2*self.b**3 - 9*self.a*self.b*self.c + 27*self.a**2 * self.d/27*self.a**3
+                p = 3*self.a*self.c -self.b/3*self.a**2
+                delta = (q/2)** 2 + (p/3)**3
+                raiz = np.cbrt(-q/2 + np.sqrt(delta)) + np.cbrt(-q/2 - np.sqrt(delta))
+                
+                self.x.append(raiz)
+                
+                x_zero = self.x[self.i]
+                print(f"a:{self.a}, b:{self.b}, c:{self.c},d:{self.d}, raiz_1: {x_zero}")
+            else:
+                self.x.append(random.randint(-1000, 1000))
+                x_zero = self.x[self.i]
+
+            print(f"Enviando valor {x_zero} para função de 3º grau")
+
+            msg = Message(to="ezioanon@jix.im", body=str(float(x_zero)), thread="unique-thread-id")
+            await self.send(msg)
+
+            response = await self.receive(timeout=20)
+            if response:
+                self.y.append(int(response.body))
+                print(f"Resultado da função de 3º grau para {self.x[self.i]}: {self.y[self.i]}")
+                if self.y[self.i] == 0:
+                    print("Resultado zero encontrado!")
+                    await self.agent.stop()
+                    self.kill()
+
+                self.i += 1
+
+    async def setup(self):
+        print("Resolvedor iniciando . . .")
+        self.add_behaviour(self.Grau())
+
 async def main():
-    resolvedor2 = Resolvedor_2grau( "ezioanon_resolvedor2@jix.im","senhamuitoforte")
-    await resolvedor2.start()
+    resolver_agent = Resolvedor("ezio_resolvedor1@jix.im", "senhamuitoforte")
+    await resolver_agent.start()
+    print("Resolvedor iniciado")
+
+    await asyncio.Event().wait()
 
 if __name__ == "__main__":
-    spade.run(main())
-a
+    asyncio.run(main())
